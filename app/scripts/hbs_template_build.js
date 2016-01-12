9d6ce95b6feab65117d65048b020f67872c76bf3
bhuber2010 <bhuber@alumni.colostate.edu>
2016-01-12 14:16:55 -0700

    not sure what changed here

--- app/scripts/hbs_template_build.js
+++ app/scripts/hbs_template_build.js
@@ -46,6 +46,8 @@ Promise.all([
 });
 
 
+
+
 // Handlebars.registerHelper('compare', function(val1, val2, options) {
 //   if (val1 == val2) return options.fn(this);
 //   else return options.inverse(this);
